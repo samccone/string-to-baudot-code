45b1dcd11a5c4932edc8a1e5eb87ccea7d8fe713
Sam Saccone <samccone@gmail.com>
2015-04-27 14:30:04 -0300

    fix: broken a encoding

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('converting strings', function() {
   it('sets the shift header (letter)', function() {
-    assert.equal(convertor('a'), '1111100101');
+    assert.equal(convertor('a'), '1111100011');
   });
 
   it('sets the shift header (unit)', function() {
@@ -11,6 +11,6 @@ describe('converting strings', function() {
   });
 
   it('toggles the shift header', function() {
-    assert.equal(convertor('a1a'), '111110010111011101111111100101');
+    assert.equal(convertor('a1a'), '111110001111011101111111100011');
   });
 });
