aa7f00db8e873dc5b1ec3726da8cf8f0fc0c8e43
Sam Saccone <samccone@gmail.com>
2015-04-27 14:30:15 -0300

    tests: Add common integration tests

--- test/index.js
+++ test/index.js
@@ -13,4 +13,20 @@ describe('converting strings', function() {
   it('toggles the shift header', function() {
     assert.equal(convertor('a1a'), '111110001111011101111111100011');
   });
+
+  it('handles a common case', function() {
+    assert.equal(convertor(
+    'hello! --world'),
+    //jscs:disable
+    '11111101000000110010100101100011011011011111100100110110001100011111111001111000010101001001001');
+    //jscs:enable
+  });
+
+  it('handles another common case', function() {
+    assert.equal(convertor(
+    'rent nine 900 times'),
+    //jscs:disable
+    '11111010100000101100100000010001100001100110000001001001101111000101101011011111001001000000110111000000100101');
+    //jscs:enable
+  });
 });
